af1a3b349a107a2008f18e55cb99387067e39f77
Chan Haeng Lee <elt@Chanui-MacBookPro.local>
2019-11-27 15:20:14 +0900

    unfinished view before CBV

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from rooms import views as room_views
 
 app_name = "core"
 
+# url은 함수만 갖고 올 수 있습니다. HomeView는 class이므로, as_view()를 사용합니다.
 urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
--- rooms/views.py
+++ rooms/views.py
@@ -15,11 +15,29 @@
 #         return redirect("/")
 
 from django.views.generic import ListView
+from django.http import Http404
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
-# ListView
+# 반복을 피하고 싶다.
+# ListView : A page representing a list of objects
 class HomeView(ListView):
 
     """ HomeView Definition """
 
+    # Room을 리스트로 나타내고 싶습니다.
     model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        # return redirect(reverse("core:home"))
+        raise Http404()
